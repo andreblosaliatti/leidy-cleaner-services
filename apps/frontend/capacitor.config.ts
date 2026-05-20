85ca23d3d9847767cf07d400201f2aa40625e634
André Blos Aliatti <andreblos82@gmail.com>
2026-05-20 15:03:23 -0300

    chore: use http scheme for local android testing

--- apps/frontend/capacitor.config.ts
+++ apps/frontend/capacitor.config.ts
@@ -4,6 +4,9 @@ const config: CapacitorConfig = {
   appId: 'br.com.leidycleaner.profissional',
   appName: 'Leidy Cleaner Profissional',
   webDir: 'dist',
+  server: {
+  androidScheme: 'http',
+},  
 };
 
 export default config;
